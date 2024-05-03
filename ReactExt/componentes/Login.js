09142d75ec802d4e1662c306b41fe1d83ecb0117
RogérioReiff <rogerioreiff@outlook.com>
2024-05-02 21:47:21 -0300

    Adicionando Login pt2 + usando asyncstorage e useState

--- ReactExt/componentes/Login.js
+++ ReactExt/componentes/Login.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity } from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Login({navigation, route}){
+
+    //TODO: O iCPF e iPswd retorno primeiro o valor nulo e depois o valor digitado pelo usuario, para CONSERTAR?
+
+    const [iCPF, setInputCPF] = useState('');
+    const [iPswd, setInputPswd] = useState('');
+    const [valueCPF, setValueCPF] = useState('');
+    const [valuePswd, setValuePswd] = useState('');
+
+    const setData = async () =>{
+        try{
+            var user = {CPF: iCPF, PASSWORD: iPswd, key: 0}
+            await AsyncStorage.setItem('UsersData', JSON.stringify(user));
+        }
+        catch(error){
+            console.log(error)
+        }
+    } 
+
+    const getData = async () =>{
+        try{
+
+           await AsyncStorage.getItem('UsersData').then(JSON.parse).then(value =>{setValueCPF(value.CPF)});
+           await AsyncStorage.getItem('UsersData').then(JSON.parse).then(value =>{setValuePswd(value.PASSWORD)});
+
+           if(valueCPF === iCPF && valuePswd === iPswd && iCPF.length !== 0){
+            route.params.funcLogar(true)
+           }else{
+            alert("Login invalido")
+            }
+        }
+        catch(error){console.log(error)}
+    }
+
+
     return(
         <View style={styles.Container}>
             <Image style={styles.Logo}
@@ -11,13 +45,14 @@ export default function Login({navigation, route}){
             <Text style={styles.textTitle}>Login</Text>
 
             <Text style={styles.textBox}>CPF</Text>
-            <TextInput style={styles.inputBox} placeholder="00000000000"/>
+            <TextInput style={styles.inputBox} placeholder="00000000000" onChangeText={(value) => {setInputCPF(value)}} autoCorrect={false} autoCapitalize='none'/>
 
             <Text style={styles.textBox}>Senha</Text>
-            <TextInput style={styles.inputBox} placeholder="Senha"/>
+            <TextInput style={styles.inputBox} placeholder="Senha" /* secureTextEntry */ onChangeText={(value)=>{setInputPswd(value)}} autoCorrect={false} autoCapitalize='none'/>
 
-            <Button title='Logar' style={{paddingTop: 10,}} onPress={()=>route.params.funcLogar(true)}/>
-            <TouchableOpacity style={{paddingTop: 10, textAlign: 'center'}}>
+            <Button title='Logar' style={{paddingTop: 10, margin: 15, alignItems: 'center', height:40}} onPress={getData}/>
+
+            <TouchableOpacity style={{paddingTop: 10, textAlign: 'center'}} onPress={setData}>
                 <Text>Esqueceu a senha?</Text>
             </TouchableOpacity>
 
@@ -30,7 +65,7 @@ export default function Login({navigation, route}){
 const styles = StyleSheet.create({
     Container:{
         flex: 1,
-        flexDirection: 'column',
+        alignContent: 'center'
     },
     Logo:{
         width: 50,
