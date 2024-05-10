9eeace2450b6e938389f9d52300e1f93ead9e8c3
RogérioReiff <rogerioreiff@outlook.com>
2024-05-10 12:01:29 -0300

    mudando posição das pastas e removido o client page do app

--- componentes/LoginToLandPage.js
+++ componentes/LoginToLandPage.js
@@ -3,8 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AdmLandingPage from './AdmLandingPage';
-import ClientLandingPage from './ClientLandingPage';
-import MenuClientPerfil from './MenuClientPerfil';
 import MenuAdmPerfil from './MenuAdmPerfil';
 
 const Drawer = createDrawerNavigator();
@@ -12,20 +10,11 @@ const Drawer = createDrawerNavigator();
 
 export default function LoginToLandPage({navigation, route}){
 
-    const [clientOrAdm, setCOA] = useState(false);
-
     return(
-      clientOrAdm?(
             <Drawer.Navigator>
                 <Drawer.Screen name="AdmLandingPage" component={AdmLandingPage}/>
                 <Drawer.Screen name="MenuAdmPerfil" component={MenuAdmPerfil} initialParams={{funcLogarB : route.params.funcLogar}}/>
             </Drawer.Navigator>
-        ):
-        (
-            <Drawer.Navigator>
-                <Drawer.Screen name="ClientLandingPage" component={ClientLandingPage}/>
-                <Drawer.Screen name="MenuClientPerfil" component={MenuClientPerfil} initialParams={{funcLogarB : route.params.funcLogar}}/>
-            </Drawer.Navigator>
-        )
+        
     );
 }
