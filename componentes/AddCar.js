98017011a1feeefe9a69c840f804615a50e5b9c0
RogérioReiff <rogerioreiff@outlook.com>
2024-05-24 23:00:28 -0300

    Adicionado AddCar, com alguns erros

--- componentes/AddCar.js
+++ componentes/AddCar.js
@@ -1,20 +1,28 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import * as FileSystem from 'expo-file-system';
+
+//POSSIVELMENTE MUDAR O ESTADO PARA UMA ESCOLHA EM VEZ DE TEXT INPUT
+//O CÓDIGO SÓ RETORNA O ÚLTIMO CARRO ADICIONADO, NÃO SEI SE ELE SOBESCREVE O OUTRO OU SE SÓ RETORNA MESMO
 
 export default function AddCar(){
 
-    let path = FileSystem.documentDirectory + 'prod1.json';
+    let path = FileSystem.documentDirectory + 'carros.json';
+    let car1 = null;
+
+    const [CPF, setCPF] = useState('');
+    const [modelo, setModelo] = useState('');
+    const [placa, setPlaca] = useState('');
+    const [estado, setEstado] = useState('');
 
     const add = async()=>{
-        let prod1 = null;
         try
         {
-            prod1 = {codigo: 1, nome: 'Teclado', quantidade: 50};
+            car1 = {codigo: Math.random(), cpf: CPF, modelo: modelo, placa: placa, estado: estado};
 
-            let prodStr = JSON.stringify(prod1);
+            let carStr = JSON.stringify(car1);
 
-            await FileSystem.writeAsStringAsync(path, prodStr, { encoding: FileSystem.EncodingType.UTF8 });
+            await FileSystem.writeAsStringAsync(path, carStr, { encoding: FileSystem.EncodingType.UTF8 });
         }
         catch(err)
         {
@@ -23,17 +31,16 @@ export default function AddCar(){
     } 
 
     const ler = async()=>{
-        let prod1 = null;
         try
         {
 
             console.log(path)
 
-            let prodStr = await FileSystem.readAsStringAsync(path);
+            let carStr = await FileSystem.readAsStringAsync(path);
 
-            prod1 = JSON.parse(prodStr);
+            car1 = JSON.parse(carStr);
 
-            console.log(prod1)
+            console.log(car1)
         }
         catch(err)
         {
@@ -47,12 +54,16 @@ export default function AddCar(){
             <Text>Adicionar Carros</Text>
 
             <Text>CPF Cliente</Text>
+            <TextInput placeholder="000.000.000-00" onChangeText={(value) => {setCPF(value)}} autoCorrect={false} autoCapitalize='none'/>
             
             <Text>Modelo</Text>
+            <TextInput placeholder="Fiat" onChangeText={(value) => {setModelo(value)}} autoCorrect={false} autoCapitalize='none'/>
 
             <Text>Placa do Carro</Text>
+            <TextInput placeholder="12-ABC-34" onChangeText={(value) => {setPlaca(value)}} autoCorrect={false} autoCapitalize='none'/>
 
             <Text>Estado</Text>
+            <TextInput placeholder="Em manutenção" onChangeText={(value) => {setEstado(value)}} autoCorrect={false} autoCapitalize='none'/>
 
             <Button title='adicionar' onPress={add}/>
             <Button title='ler' onPress={ler}/>
